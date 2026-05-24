9dc7fd842a8b9e0acca1d9ddbdc0123c44b9c80b
vikandula <vijay.kandula@gmail.com>
2026-05-24 18:10:25 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+temples = [
+    {
+        "name": "Badrinath Temple",
+        "location": "Uttarakhand",
+        "deity": "Lord Badrinath (Vishnu)",
+        "description": "One of the Char Dham pilgrimage sites, nestled in the Garhwal hill tracks in the Himalayas.",
+        "best_time": "May to October (Closed in winter)"
+    },
+    {
+        "name": "Sri Venkateswara Swami Temple",
+        "location": "Tirupati, Andhra Pradesh",
+        "deity": "Lord Venkateswara",
+        "description": "Also known as Tirupati Balaji, it is one of the most visited and wealthiest temples in the world.",
+        "best_time": "September to February"
+    },
+    {
+        "name": "Jagannath Temple",
+        "location": "Puri, Odisha",
+        "deity": "Lord Jagannath",
+        "description": "Famous for its annual Rath Yatra (Chariot Festival). Another key Char Dham site.",
+        "best_time": "June to March"
+    },
+    {
+        "name": "Sri Ranganathaswamy Temple",
+        "location": "Srirangam, Tamil Nadu",
+        "deity": "Lord Ranganatha",
+        "description": "The largest functioning Hindu temple complex in the world, dedicated to the reclining form of Lord Vishnu.",
+        "best_time": "October to March"
+    },
+    {
+        "name": "Dwarkadhish Temple",
+        "location": "Dwarka, Gujarat",
+        "deity": "Lord Krishna (Dwarkadhish)",
+        "description": "An ancient temple dedicated to Lord Krishna, the king of Dwarka. Part of the Char Dham.",
+        "best_time": "October to March"
+    }
+]
+
+@app.route('/')
+def home():
+    return render_template('index.html', temples=temples)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
